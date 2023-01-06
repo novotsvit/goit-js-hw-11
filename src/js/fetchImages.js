f42e7803155f332790b8a5c84e0eeb0df2b8c86a
Альона <108233197+novotsvit@users.noreply.github.com>
2023-01-06 14:47:24 +0200

    red-2

--- src/js/fetchImages.js
+++ src/js/fetchImages.js
@@ -2,6 +2,7 @@ export const fetchImages = async (inputValue, pageNr) => {
   return await fetch(
     `https://pixabay.com/api/?key=29588079-fbc492831fdad231bf7222b96&q=${inputValue}&orientation=horizontal&safesearch=true&image_type=photo&per_page=40&page=${pageNr}`
   )
+    
     .then(async response => {
       if (!response.ok) {
         if (response.status === 404) {
